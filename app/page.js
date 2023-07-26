fa561e32dca40aa8b93623fda6c495ed6552a2cb
los <thiscarlos@gmail.com>
2023-07-26 16:27:58 -0700

    background width adjusted

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { cinzel } from './fonts'
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-full flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl after:absolute after:-z-20 after:h-[180px]">
+      <div className="relative max-w-5xl flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl after:absolute after:-z-20 after:h-[180px]">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert"
           src="/Kots-poiretone-nobg.svg"
