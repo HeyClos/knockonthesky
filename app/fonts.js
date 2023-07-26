b64f9e87a3127ce51707631d92182d60555d3b60
los <thiscarlos@gmail.com>
2023-07-26 15:43:29 -0700

    Added Styling, fonts, and logo to landing page

---
+++ app/fonts.js
@@ -0,0 +1,11 @@
+import { Cinzel, Bodoni_Moda } from 'next/font/google'
+ 
+export const cinzel = Cinzel({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const bodoni_moda = Bodoni_Moda({
+  subsets: ['latin'],
+  display: 'swap',
+})
