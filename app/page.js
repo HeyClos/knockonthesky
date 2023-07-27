dc85a52038f0b8edd34ad672341b294890bdd80c
los <thiscarlos@gmail.com>
2023-07-27 16:18:09 -0700

    body padding update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { cinzel } from './fonts'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-screen	flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen w-screen	flex-col items-center justify-between py-14">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert"
