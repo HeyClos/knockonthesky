ef7e5cb4d2794c8ed9516ea25346fa2390bb74e6
los <thiscarlos@gmail.com>
2023-07-27 14:52:10 -0700

    bg width set to screen

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { cinzel } from './fonts'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-w-fit flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen w-screen	flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl after:absolute after:-z-20 after:h-[180px]">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert"
