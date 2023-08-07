9fd7186c2d8a17dd4df6ce19b84ddde2e59efbb2
los <thiscarlos@gmail.com>
2023-08-07 15:05:02 -0700

    daisyui installed

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { cinzel } from './fonts'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-screen	flex-col items-center justify-between py-14">
+    <main className="flex min-h-screen w-screen	flex-col items-center py-14 justify-between">
       <div id="banner" className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert"
@@ -17,7 +17,21 @@ export default function Home() {
       <h1 id="tagline" className={cinzel.className}>
         & listen to the sound
       </h1>
-      <div className="text-yellow-100/60 text-sm">
+      <div className="py-14">
+        Our Services
+        <div className="py-4 justify-between grid-cols-2">
+          <div className="p-4">
+            For Musicians
+          </div>
+          <div className="p-4">
+            For Tv/Film/Ad producers
+          </div>
+          <div className="p-4">
+            For Students & Non profits
+          </div>
+        </div>
+      </div>      
+      <div className="text-yellow-100/60 text-sm flex">
         coming soon...
       </div>
     </main>
