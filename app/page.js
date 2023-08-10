9a3d69c668235d59484ab52d8681f95a0a9e2d06
los <thiscarlos@gmail.com>
2023-08-10 13:32:55 -0700

    edited background blur and removed CTA buttons

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col place-items-center p-14 justify-between">
       <div className="hero h-5/6">
         <div className="hero-content text-center">
-          <div className="max-w-md">
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl">
+          <div className="max-w-xl">
+            <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-6/12 before:rounded-full before:bg-gradient-radial before:from-slate-400/60 before:to-slate-500/50 before:blur-3xl">
               <Image
                 className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert"
                 src="/Kots-poiretone-nobg.svg"
@@ -16,11 +16,12 @@ export default function Home() {
                 height={87}
                 priority
               />
+              <h1 id="tagline" className={cinzel.className}>
+                & listen to the sound
+              </h1>
             </div>
-            <h1 id="tagline" className={cinzel.className}>
-              & listen to the sound
-            </h1>
-            <p className="py-12 text-sm">KOTS is a music organization that helps exceptional artists build sustainable careers. We believe that by helping artists evolve their human experience internally, they can radiate that experience onto others via their art and their everyday lives.</p>
+
+            <p className={`${cinzel.className} py-12 text-sm text-lime-300 tracking-widest`}>KOTS is a music organization that helps exceptional artists build sustainable careers. We believe that by helping artists evolve their human experience internally, they can radiate that experience onto others via their art and their everyday lives.</p>
           </div>
         </div>
       </div>
@@ -32,7 +33,6 @@ export default function Home() {
             <h2 className="card-title">For Ad/TV/Film producers</h2>
             <p>Award-winning music composition for Advertisements, TV shows, and Film. High-quality, quick turnarounds. </p>
             <div className="card-actions justify-end">
-              <button className="btn glass invert">Book us today.</button>
             </div>
           </div>
         </div>
@@ -41,7 +41,6 @@ export default function Home() {
             <h2 className="card-title">For Artists</h2>
             <p>Music production & management services. Including studio sessions, engineering, marketing, representation and more.</p>
             <div className="card-actions justify-end">
-              <button className="btn glass invert">Book us today.</button>
             </div>
           </div>
         </div>
@@ -51,7 +50,6 @@ export default function Home() {
             <h2 className="card-title">For Students & Non profits</h2>
             <p>We're happy to contribute our work pro-bono and help take your work & projects to the next level.</p>
             <div className="card-actions justify-end">
-              <button className="btn glass invert">Coming Soon...</button>
             </div>
           </div>
         </div>
